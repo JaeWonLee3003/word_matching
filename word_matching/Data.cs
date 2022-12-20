3f6cfadde88c6466e5447bda4602e5e854f9ea17
Administrator <Administrator@309-14>
2022-12-20 10:13:39 +0900

    초기 커밋.

---
+++ word_matching/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace word_matching
+{
+    public class data
+    {
+
+
+        public class allsetData
+        {
+
+        }
+
+        public class result
+        {
+
+        }
+    }
+}
---
+++ word_matching/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace word_matching
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+
+    // 입력 메서드
+
+    // 출력 메서드
+}
